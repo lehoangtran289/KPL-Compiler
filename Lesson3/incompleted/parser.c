4b109e0061048e692ddff5eea3b2413bd5ae0f46
lehoangtran289 <lehoangtran289@gmail.com>
2020-11-04 21:53:04 +0700

    fix bug: invalid basictype for float number

--- Lesson3/incompleted/parser.c
+++ Lesson3/incompleted/parser.c
@@ -326,6 +326,9 @@ void compileBasicType(void) {
         case KW_INTEGER:
             eat(KW_INTEGER);
             break;
+        case KW_FLOAT:
+            eat(KW_FLOAT);
+            break;
         case KW_CHAR:
             eat(KW_CHAR);
             break;
